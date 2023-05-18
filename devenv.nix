8f703bef6bfc4bbda59da3f8d76ae1032ec20847
Michał Kiełbowicz <git@kielbowi.cz>
2023-05-19 01:04:20 +0200

    Add support for devenv (#2)
    Devenv is a tool that setups development environment by installing
    packages, configuring them, managing services, running processes, etc.
    Initial definition takes care of flutter, JDK and Android SDK.

---
+++ devenv.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }:
+
+let
+  android-comp     = pkgs.androidenv.composeAndroidPackages {
+    buildToolsVersions = [ "30.0.3" ];
+    platformVersions   = [ "29" "30" "31" "33" ];
+  };
+  android-sdk      = android-comp.androidsdk;
+  android-sdk-root = "${android-sdk}/libexec/android-sdk";
+in {
+  env = {
+    ANDROID_HOME     = "${android-sdk-root}";
+    ANDROID_SDK_ROOT = "${android-sdk-root}";
+  };
+
+  languages.java = {
+    enable      = true;
+    jdk.package = pkgs.jdk11;
+  };
+
+  name = "mm-flutter-app";
+
+  packages = [ android-sdk ];
+}
