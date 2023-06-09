4e0f423a9dd18438d852974da3db523db013c2f6
Ashley Liu <lingyul1998@gmail.com>
2023-06-09 11:21:51 -0400

    Remove widgetbook code due to widgetbook / go_router conflict

--- widgetbook/screens/tabscreen/tabs_component.dart
+++
@@ -1,8 +0,0 @@
-import 'package:widgetbook/widgetbook.dart';
-import 'use_cases/tabs.dart';
-
-WidgetbookComponent tabsComponent() {
-  return WidgetbookComponent(name: 'Tabs', useCases: [
-    tabs(),
-  ]);
-}
--- widgetbook/screens/welcome/welcome_screen.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:mm_flutter_app/widgets/screens/welcome/welcome_screen.dart';
-
-Widget welcomeScreen({authToken, userId, deviceId}) {
-  return const WelcomeScreen();
-}
