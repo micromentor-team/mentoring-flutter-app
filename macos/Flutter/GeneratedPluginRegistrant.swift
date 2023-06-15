c059b8f36c2197bf4b1d24ddae9868ff69a15d4b
Miguel Rosario <miguel.a.rosario.aponte@gmail.com>
2023-06-15 15:22:58 -0700

    Add Firebase Core and Crashlytics integration
    Note that the google-services.json (Android) and GoogleService-Info.plist (iOS) are not included here because these are to be generated using the Firebase CLI to retrieve them from the project hosted in Firebase. These can be added to version control, but they expose our API Key and is not ideal for an Open Source project.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,8 @@ import Foundation
 
 import connectivity_plus
 import emoji_picker_flutter
+import firebase_core
+import firebase_crashlytics
 import path_provider_foundation
 import shared_preferences_foundation
 import url_launcher_macos
@@ -14,6 +16,8 @@ import url_launcher_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   EmojiPickerFlutterPlugin.register(with: registry.registrar(forPlugin: "EmojiPickerFlutterPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
