702534af11d9938890535d3e283fcc759e76fc56
Miguel Rosario <miguel.a.rosario.aponte@gmail.com>
2023-06-20 16:39:37 -0700

    Basic UI for Invitations section
    Missing the 'Show all' button, logic to limit the number of tiles to show, and hide/show elements dynamically.

---
+++ lib/widgets/atoms/invitation_tile.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:mm_flutter_app/constants/app_constants.dart';
+
+enum InvitationTileType {
+  requested,
+  accepted,
+}
+
+class InvitationTile extends StatelessWidget {
+  final String userName;
+  final String userJobTitle;
+  final InvitationTileType invitationTileType;
+  final String? avatarUrl;
+  final void Function() buttonOnPressed;
+
+  const InvitationTile({
+    Key? key,
+    required this.userName,
+    required this.userJobTitle,
+    required this.invitationTileType,
+    this.avatarUrl,
+    required this.buttonOnPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final AppLocalizations l10n = AppLocalizations.of(context)!;
+    return ListTile(
+      minVerticalPadding: Insets.widgetMediumLargeInset,
+      leading: CircleAvatar(
+        radius: Radii.avatarRadiusSmall,
+        backgroundImage: avatarUrl != null ? NetworkImage(avatarUrl!) : null,
+      ),
+      title: Text(
+        userName,
+        maxLines: 1,
+        overflow: TextOverflow.ellipsis,
+      ),
+      titleTextStyle: TextStyles.cardTitle(context),
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: Insets.widgetSmallestInset,
+            ),
+            child: Text(
+              userJobTitle,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+          Text(
+            _invitationText(l10n),
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+          ),
+        ],
+      ),
+      subtitleTextStyle: TextStyles.mentorCardSubtitle(context),
+      trailing: SizedBox(
+        width: Radii.avatarRadiusSmall * 2,
+        height: Radii.avatarRadiusSmall * 2,
+        child: ElevatedButton(
+          onPressed: buttonOnPressed,
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.all(0),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(Radii.avatarRadiusSmall),
+            ),
+          ),
+          child: const Icon(
+            Icons.navigate_next,
+          ),
+        ),
+      ),
+    );
+  }
+
+  String _invitationText(AppLocalizations l10n) {
+    switch (invitationTileType) {
+      case InvitationTileType.requested:
+        return l10n.homeInvitationRequested;
+      case InvitationTileType.accepted:
+        return l10n.homeInvitationAccepted;
+    }
+  }
+}
--- lib/widgets/atoms/section_tile.dart
+++ lib/widgets/atoms/section_tile.dart
@@ -4,11 +4,15 @@ import 'package:mm_flutter_app/constants/app_constants.dart';
 class SectionTile extends StatelessWidget {
   final String title;
   final Widget child;
+  final bool addTopDivider;
+  final bool removeBottomPadding;
 
   const SectionTile({
     Key? key,
     required this.title,
     required this.child,
+    this.addTopDivider = false,
+    this.removeBottomPadding = false,
   }) : super(key: key);
 
   @override
@@ -16,6 +20,7 @@ class SectionTile extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
+        if (addTopDivider) Components.indentedDivider,
         Padding(
           padding: const EdgeInsets.all(Insets.widgetMediumInset),
           child: Text(
@@ -24,7 +29,9 @@ class SectionTile extends StatelessWidget {
           ),
         ),
         Padding(
-          padding: const EdgeInsets.only(bottom: Insets.widgetMediumInset),
+          padding: EdgeInsets.only(
+            bottom: removeBottomPadding ? 0 : Insets.widgetMediumInset,
+          ),
           child: Center(
             child: child,
           ),
