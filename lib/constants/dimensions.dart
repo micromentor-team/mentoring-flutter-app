ba4b5fab1c1ac55481830e164dbe97847720ee00
Rupal Gupta <rupalgupta99@gmail.com>
2023-07-11 18:51:08 +0530

    moving icons 30 pixels left

--- lib/constants/dimensions.dart
+++ lib/constants/dimensions.dart
@@ -43,5 +43,5 @@ class Dimensions {
   static const double chipMaxWidth = 100;
   static const double loginBoxWidth = 360;
   static const double lineHeight = 1;
-  static const double textBoxWidth = 170;
+  static const double textBoxWidth = 200;
 }
