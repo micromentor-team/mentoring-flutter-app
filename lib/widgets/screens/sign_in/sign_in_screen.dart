9ff8f9fc092751a7a9f541b01889a98cd317a0c6
Rupal Gupta <rupalgupta99@gmail.com>
2023-07-13 19:32:31 +0530

    adding user provider

--- lib/widgets/screens/sign_in/sign_in_screen.dart
+++ lib/widgets/screens/sign_in/sign_in_screen.dart
@@ -1,6 +1,7 @@
 import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:mm_flutter_app/data/models/user/user_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import '../../../__generated/schema/schema.graphql.dart';
