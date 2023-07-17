a32a2aed854c50373133a51c93a26f955f6ffa5c
jupblb <jupblb@google.com>
2023-07-17 17:03:15 +0200

    Make it possible to run mock server via devenv up

--- devenv.nix
+++ devenv.nix
@@ -1,7 +1,10 @@
 { pkgs, ... }:
 
 let
-  android-comp     = android-pkgs.androidenv.composeAndroidPackages {
+  androidenv       = android-pkgs.androidenv.override {
+    licenseAccepted = true;
+  };
+  android-comp     = androidenv.composeAndroidPackages {
     buildToolsVersions = [ "30.0.3" ];
     platformVersions   = [ "29" "30" "31" "33" ];
   };
@@ -18,13 +21,25 @@ in {
     ANDROID_SDK_ROOT = "${android-sdk-root}";
   };
 
-  languages.java = {
-    enable      = true;
-    jdk.package = pkgs.jdk11;
+  languages = {
+    java = {
+      enable      = true;
+      jdk.package = pkgs.jdk11;
+    };
+
+    javascript.enable = true;
   };
 
   name = "mm-flutter-app";
 
   packages = with pkgs;
     [ act android-sdk flutter nodePackages.firebase-tools pandoc ];
+
+  processes.mock-server.exec = ''
+    mv assets/.env assets/.env.bak 2>/dev/null || true
+    touch assets/.env
+    echo APP_GRAPHQL_URL="http://localhost:4000/mmdata/api/graphql" >> assets/.env
+    echo APP_SUBSCRIPTION_URL="ws://localhost:4000/mmdata/api/graphql" >> assets/.env
+    cd mm-mock-server && npm install && npm start
+  '';
 }
