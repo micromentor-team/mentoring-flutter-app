1269cb6ee421534bd734029ff0195ef91fdc0d2d
chris anderson <andersan@umich.edu>
2023-08-02 10:26:18 -0500

    Add "searches" for mentor/mentee users

--- lib/providers/content_provider.dart
+++ lib/providers/content_provider.dart
@@ -1,12 +1,12 @@
-import 'package:flutter/material.dart';
-import 'package:graphql_flutter/graphql_flutter.dart';
-import 'package:mm_flutter_app/__generated/schema/operations_content.graphql.dart';
-import 'package:mm_flutter_app/__generated/schema/schema.graphql.dart';
+// import 'package:flutter/material.dart';
+// import 'package:graphql_flutter/graphql_flutter.dart';
+// import 'package:mm_flutter_app/__generated/schema/operations_content.graphql.dart';
+// import 'package:mm_flutter_app/__generated/schema/schema.graphql.dart';
 
-import 'base/base_provider.dart';
-import 'base/operation_result.dart';
+// import 'base/base_provider.dart';
+// import 'base/operation_result.dart';
 
-class ContentProvider extends BaseProvider {
-  ContentProvider({required super.client});
-  // TODO: add queries for expertises, education levels, countries, company stages, company types, genders, industries, and languages
-}
+// class ContentProvider extends BaseProvider {
+//   ContentProvider({required super.client});
+//   // TODO: add queries for expertises, education levels, countries, company stages, company types, genders, industries, and languages
+// }
