85b3d6d7c6ee241ae3c787acf7b4b0cfa1202a8f
Ashley <100324646+ashlilili@users.noreply.github.com>
2023-08-04 14:44:57 -0400

    Profile basic info section (#79)
    1. Profile basic info section; displays vacation banner, user type,
    affiliations, company, education, etc.
    2. moved three dots menu for block/report user to a separate file for
    code reuse
    3. extended profileChip class to have an option to use primary color
    
    Note: 
    The vacation banner color may seem odd with profile page header, as
    figma uses primary95, which is not in the color scheme, so I'm using the
    closest color primaryContainer instead. I will follow up with Katy on
    this.
    
    ---
    
    
    https://github.com/micromentor-team/mm-flutter-app/assets/100324646/5a6c30f5-389e-4b5a-973b-579d2227d13f
    
    
    
    
    
    All fields
    ----
    
    <img width="500" alt="Screenshot 2023-08-02 at 5 04 51 PM"
    src="https://github.com/micromentor-team/mm-flutter-app/assets/100324646/69180d8b-13d2-4514-a538-68de929fdf48">
    Only mandatory fields
    
    ---------
    
    Co-authored-by: Miguel Rosario <miguel.a.rosario.aponte@gmail.com>

--- lib/widgets/atoms/profile_chip.dart
+++ lib/widgets/atoms/profile_chip.dart
@@ -5,11 +5,13 @@ import '../../constants/app_constants.dart';
 class ProfileChip extends StatelessWidget {
   final String text;
   final IconData? icon;
+  final bool? usePrimaryColor;
 
   const ProfileChip({
     super.key,
     required this.text,
     this.icon,
+    this.usePrimaryColor,
   });
 
   @override
@@ -25,11 +27,15 @@ class ProfileChip extends StatelessWidget {
       label: Text(
         text,
         style: theme.textTheme.labelSmall?.copyWith(
-          color: theme.colorScheme.onSecondaryContainer,
+          color: (usePrimaryColor != null && usePrimaryColor!)
+              ? theme.colorScheme.onSurfaceVariant
+              : theme.colorScheme.onSecondaryContainer,
         ),
         overflow: TextOverflow.ellipsis,
       ),
-      backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
+      backgroundColor: (usePrimaryColor != null && usePrimaryColor!)
+          ? theme.colorScheme.inversePrimary
+          : theme.colorScheme.secondaryContainer,
       side: BorderSide.none,
       visualDensity: const VisualDensity(
         horizontal: VisualDensity.minimumDensity,
