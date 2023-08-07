dd8a94f2f088dad0338e0ff1527ea4218e26a899
chris anderson <andersan@umich.edu>
2023-08-07 15:23:46 -0500

    Add content and some group logic to mock server

--- mm-mock-server/src/mocks/queries.ts
+++ mm-mock-server/src/mocks/queries.ts
@@ -1,19 +1,10 @@
 import { MockServerState } from "./util/state";
+import * as constants from "./util/constants";
 import * as generators from "./util/generators";
 
 export function mockQueries(serverState: MockServerState) {
     return {
-        findChannelById: () => {
-            var channel = serverState.channels[0];
-            channel.latestMessage = serverState.channelMessages[serverState.channelMessages.length-1];
-            return channel;
-        },
-        findChannelMessages: () => {
-            return serverState.channelMessages;
-        },
-        findChannelsForUser: () => {
-            return serverState.channels;
-        },
+        // users
         findUsers: () => {
             return serverState.otherUsers.concat([serverState.loggedInUser]);
         },
@@ -22,6 +13,18 @@ export function mockQueries(serverState: MockServerState) {
                 return null;
             }
             return serverState.loggedInUser;
+        },
+        // channels, channel invitations and channel messages
+        findChannelById: () => {
+            var channel = serverState.channels[0];
+            channel.latestMessage = serverState.channelMessages[serverState.channelMessages.length - 1];
+            return channel;
+        },
+        findChannelMessages: () => {
+            return serverState.channelMessages;
+        },
+        findChannelsForUser: () => {
+            return serverState.channels;
         },
         myInbox: () => {
             return {
@@ -34,11 +37,40 @@ export function mockQueries(serverState: MockServerState) {
                         serverState.channelInboxItemInvitations[1],
                     ],
                     unseenMessages: [
-                        generators.generateChannelInboxItemMessage(serverState.channels[0].id, serverState.loggedInUser),
-                        generators.generateChannelInboxItemMessage(serverState.channels[0].id, serverState.otherUsers[0]),
+                        generators.generateChannelInboxItemMessage(serverState.channels[0], serverState.loggedInUser),
+                        generators.generateChannelInboxItemMessage(serverState.channels[0], serverState.otherUsers[0]),
                     ],
                 }
             }
+        },
+        // content service
+        findCompanyStages: () => {
+            return constants.companyStages;
+        },
+        findCompanyTypes: () => {
+            return constants.companyTypes;
+        },
+        findCountries: () => {
+            return constants.countries;
+        },
+        findEducationLevels: () => {
+            return constants.educationLevels;
+        },
+        findExpertises: () => {
+            return constants.expertises;
+        },
+        findGenders: () => {
+            return constants.genders;
+        },
+        findIndustries: () => {
+            return constants.industries;
+        },
+        findLanguages: () => {
+            return constants.languages;
+        },
+        // groups
+        findGroups: () => {
+            return serverState.groups;
         },
     }
 }
