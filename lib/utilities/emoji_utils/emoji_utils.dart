072835183f3a2bc5379d60158484d7e99eba7964
Miguel Rosario <miguel.a.rosario.aponte@gmail.com>
2023-08-25 16:01:29 -0700

    Add Emoji utility to separate text and emoji (to be used later)

--- lib/utilities/emoji_utils/emoji_utils.dart
+++ lib/utilities/emoji_utils/emoji_utils.dart
@@ -4,19 +4,19 @@
 import 'package:flutter/material.dart';
 
 class EmojiUtils {
+  EmojiUtils._private();
+  static final RegExp emojiRegExp = RegExp(
+      r"(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])");
+
   static bool containsEmojis(String text) {
     if (text.isEmpty) return false;
-    RegExp regExp = RegExp(
-        r"(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])");
-    return regExp.hasMatch(text);
+    return emojiRegExp.hasMatch(text);
   }
 
-  static bool strictlyEmojis(String text) {
+  static bool isOnlyEmoji(String text) {
     if (text.isEmpty) return false;
-    RegExp regExp = RegExp(
-        r"(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])");
     final charList =
-        Characters(text).where((p0) => !regExp.hasMatch(p0)).toList();
+        Characters(text).where((p0) => !emojiRegExp.hasMatch(p0)).toList();
     if (charList.isNotEmpty) {
       return false;
     } else {
@@ -26,24 +26,20 @@ class EmojiUtils {
 
   static int emojiCount(String text) {
     if (text.isEmpty) return 0;
-    RegExp regExp = RegExp(
-        r"(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])");
     final emojiCount =
-        Characters(text).where((p0) => regExp.hasMatch(p0)).length;
+        Characters(text).where((p0) => emojiRegExp.hasMatch(p0)).length;
     return emojiCount;
   }
 
   static List getCharacterList(String text,
       {bool emojiOnly = false, bool sansEmoji = false}) {
     if (text.isEmpty) return Characters(text).toList();
-    RegExp regExp = RegExp(
-        r"(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])");
     final charList =
-        Characters(text).where((p0) => !regExp.hasMatch(p0)).toList();
+        Characters(text).where((p0) => !emojiRegExp.hasMatch(p0)).toList();
     final sansEmojiList =
-        Characters(text).where((p0) => !regExp.hasMatch(p0)).toList();
+        Characters(text).where((p0) => !emojiRegExp.hasMatch(p0)).toList();
     final emojiList =
-        Characters(text).where((p0) => regExp.hasMatch(p0)).toList();
+        Characters(text).where((p0) => emojiRegExp.hasMatch(p0)).toList();
     if (emojiOnly) {
       return emojiList;
     } else if (sansEmoji) {
@@ -52,4 +48,59 @@ class EmojiUtils {
       return charList;
     }
   }
+
+  static List<MessageFragment> splitTextAndEmoji(String message) {
+    List<MessageFragment> messageFragments = [];
+    String currentFragmentText = '';
+    // Iterate over each character in the message and create fragments of only
+    // text and only emoji.
+    final Characters chars = Characters(message);
+    for (String char in chars) {
+      if (emojiRegExp.hasMatch(char)) {
+        if (currentFragmentText.isNotEmpty &&
+            !emojiRegExp.hasMatch(currentFragmentText)) {
+          messageFragments.add(
+            MessageFragment(
+              text: currentFragmentText,
+              isEmoji: false,
+            ),
+          );
+          currentFragmentText = '';
+        }
+        currentFragmentText += char;
+      } else {
+        if (currentFragmentText.isNotEmpty &&
+            emojiRegExp.hasMatch(currentFragmentText)) {
+          messageFragments.add(
+            MessageFragment(
+              text: currentFragmentText,
+              isEmoji: true,
+            ),
+          );
+          currentFragmentText = '';
+        }
+        currentFragmentText += char;
+      }
+    }
+
+    if (currentFragmentText.isNotEmpty) {
+      messageFragments.add(
+        MessageFragment(
+          text: currentFragmentText,
+          isEmoji: isOnlyEmoji(currentFragmentText),
+        ),
+      );
+    }
+    return messageFragments;
+  }
+}
+
+class MessageFragment {
+  final String text;
+  final bool isEmoji;
+
+  MessageFragment({
+    required this.text,
+    required this.isEmoji,
+  });
 }
