37fea1984080a2fc8db5116d162a3e35ab26b516
Miguel Rosario <miguel.a.rosario.aponte@gmail.com>
2023-09-05 15:57:59 -0700

    Show list of pending sent invitations on the Inbox

--- lib/widgets/screens/inbox/inbox_invites_sent.dart
+++ lib/widgets/screens/inbox/inbox_invites_sent.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:mm_flutter_app/constants/app_constants.dart';
+import 'package:mm_flutter_app/providers/invitations_provider.dart';
+import 'package:mm_flutter_app/providers/models/my_channel_invitations_model.dart';
 import 'package:mm_flutter_app/utilities/router.dart';
+import 'package:mm_flutter_app/utilities/utility.dart';
+import 'package:mm_flutter_app/widgets/atoms/empty_state_message.dart';
 import 'package:mm_flutter_app/widgets/molecules/inbox_list_tile.dart';
 import 'package:provider/provider.dart';
 
 import '../../../providers/models/scaffold_model.dart';
-import '../../../utilities/debug_logger.dart';
 
 class InboxInvitesSentScreen extends StatefulWidget {
   const InboxInvitesSentScreen({super.key});
@@ -17,65 +20,25 @@ class InboxInvitesSentScreen extends StatefulWidget {
 
 class _InboxInvitesSentScreenState extends State<InboxInvitesSentScreen>
     with RouteAwareMixin<InboxInvitesSentScreen> {
+  late final MyChannelInvitationsModel _myChannelInvitationsModel;
+  late AppLocalizations _l10n;
+
   static const int tabBarIndex = 1;
 
-  List<InboxListTile> _createTestTiles(AppLocalizations l10n) {
-    // TODO(m-rosario): Replace mock data with backend data.
-    return [
-      InboxListTile(
-        avatarUrl:
-            'https://media.istockphoto.com/id/1160811064/photo/portrait-of-a-handsome-latin-man.jpg?s=612x612&w=0&k=20&c=MxkLwUFZ9ChfzFdB-OmmiWBnZrSioj9MmfSdlwCk4-4=',
-        fullName: 'Antoine Mousa',
-        date: DateTime.now(),
-        message:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        highlightTile: true,
-        simplifyDate: true,
-        datePrefix: l10n.inboxInvitesDateSent,
-        onPressed: () => DebugLogger.warning('TODO: NOT IMPLEMENTED.'),
-      ),
-      InboxListTile(
-        avatarUrl:
-            'https://st4.depositphotos.com/9999814/39958/i/600/depositphotos_399584092-stock-photo-attractive-young-woman-profile-portrait.jpg',
-        fullName: 'Azadeh Sana',
-        date: DateTime.now().subtract(const Duration(days: 1)),
-        message:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        highlightTile: true,
-        simplifyDate: true,
-        datePrefix: l10n.inboxInvitesDateSent,
-        onPressed: () => DebugLogger.warning('TODO: NOT IMPLEMENTED.'),
-      ),
-      InboxListTile(
-        avatarUrl:
-            'https://media.istockphoto.com/id/1307694427/photo/portrait-of-businessman-in-glasses-holding-smartphone-in-hand.jpg?s=612x612&w=0&k=20&c=P4FDNemdXlXQi3O_yLePrJVzuTYmJx84-iIySj91fGQ=',
-        fullName: 'Jenika Chua',
-        date: DateTime.now().subtract(const Duration(days: 7)),
-        message:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        highlightTile: true,
-        simplifyDate: true,
-        datePrefix: l10n.inboxInvitesDateSent,
-        onPressed: () => DebugLogger.warning('TODO: NOT IMPLEMENTED.'),
-      ),
-    ];
+  @override
+  void initState() {
+    super.initState();
+    _myChannelInvitationsModel = Provider.of<MyChannelInvitationsModel>(
+      context,
+      listen: false,
+    );
   }
 
-  List<Widget> _createContentList(List<InboxListTile> tiles) {
-    if (tiles.isEmpty) {
-      return [];
-    }
-    List<Widget> contentList = [tiles.first];
-    for (int i = 1; i < tiles.length; i++) {
-      contentList.addAll([
-        const Divider(
-          height: 0,
-          indent: Insets.paddingSmall,
-        ),
-        tiles[i],
-      ]);
-    }
-    return contentList;
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _myChannelInvitationsModel.refreshSentInvitations(onlyPending: true);
+    _l10n = AppLocalizations.of(context)!;
   }
 
   void _refreshScaffold() {
@@ -107,21 +70,82 @@ class _InboxInvitesSentScreenState extends State<InboxInvitesSentScreen>
     }
   }
 
+  InboxListTile _createTile(
+    SentChannelInvitation invitation,
+  ) {
+    return InboxListTile(
+      avatarUrl: invitation.recipient.avatarUrl,
+      fullName: invitation.recipient.fullName ?? '',
+      date: invitation.createdAt.toLocal(),
+      message: invitation.messageText ?? '',
+      highlightTile: false,
+      simplifyDate: true,
+      onPressed: () => router.push(
+        '${Routes.inboxInvitesSent.path}/${invitation.id}',
+      ),
+    );
+  }
+
+  List<InboxListTile> _createTileList(
+    List<SentChannelInvitation> invitations,
+  ) {
+    List<InboxListTile> tiles = [];
+    for (SentChannelInvitation invitation in invitations) {
+      tiles.add(
+        _createTile(
+          invitation,
+        ),
+      );
+    }
+    return tiles;
+  }
+
+  List<Widget> _createContentList(List<InboxListTile> tiles) {
+    if (tiles.isEmpty) {
+      return [];
+    }
+    List<Widget> contentList = [tiles.first];
+    for (int i = 1; i < tiles.length; i++) {
+      contentList.addAll([
+        const Divider(
+          height: 0,
+          indent: Insets.paddingSmall,
+        ),
+        tiles[i],
+      ]);
+    }
+    return contentList;
+  }
+
   @override
   Widget build(BuildContext context) {
-    final AppLocalizations l10n = AppLocalizations.of(context)!;
-    return SafeArea(
-      child: Padding(
-        padding: const EdgeInsetsDirectional.only(
-          start: Insets.paddingSmall,
-          end: Insets.paddingMedium,
-        ),
-        child: ListView(
-          children: _createContentList(
-            _createTestTiles(l10n),
-          ),
-        ),
-      ),
+    return Consumer<MyChannelInvitationsModel>(
+      builder: (context, myChannelInvitationsModel, _) {
+        return AppUtility.widgetForAsyncState(
+          state: myChannelInvitationsModel.state,
+          onReady: () {
+            if (myChannelInvitationsModel.sentInvitations.isEmpty) {
+              return EmptyStateMessage(
+                icon: Icons.mail,
+                text: _l10n.emptyStateInvites,
+              );
+            }
+            return Padding(
+              padding: const EdgeInsetsDirectional.only(
+                start: Insets.paddingSmall,
+                end: Insets.paddingMedium,
+              ),
+              child: ListView(
+                children: _createContentList(
+                  _createTileList(
+                    myChannelInvitationsModel.sentInvitations,
+                  ),
+                ),
+              ),
+            );
+          },
+        );
+      },
     );
   }
 }
