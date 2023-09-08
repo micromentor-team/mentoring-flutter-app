40fc55536faf57eaf291b3bf8af6f27ec6b7ebf1
Miguel Rosario <miguel.a.rosario.aponte@gmail.com>
2023-09-08 10:03:32 -0700

    Remove user type selection from signup navigation

--- lib/widgets/screens/sign_up/sign_up_email.dart
+++ lib/widgets/screens/sign_up/sign_up_email.dart
@@ -22,53 +22,58 @@ class _SignUpEmailState extends State<SignUpEmail> {
     final AppLocalizations l10n = AppLocalizations.of(context)!;
 
     return Scaffold(
-        body: SafeArea(
-            child: Padding(
-                padding: const EdgeInsets.all(Insets.paddingExtraLarge),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      const LinearProgressIndicator(value: 0.25),
-                      const SizedBox(height: Insets.paddingMedium),
-                      Text(
-                        l10n.whatIsYourEmailAddress,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(Insets.paddingExtraLarge),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              const LinearProgressIndicator(value: 0.25),
+              const SizedBox(height: Insets.paddingMedium),
+              Text(
+                l10n.whatIsYourEmailAddress,
+                softWrap: true,
+                style: theme.textTheme.headlineSmall?.copyWith(
+                  color: theme.colorScheme.primary,
+                ),
+              ),
+              const SizedBox(height: Insets.paddingMedium),
+              TextFormFieldWidget(
+                  textController: emailTextController,
+                  label: l10n.emailAddress,
+                  onPressed: (value) {
+                    setState(() {
+                      email = value;
+                    });
+                  },
+                  obscureText: false),
+              const Spacer(),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: Insets.paddingExtraLarge * 2),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(Icons.lock_outline, color: theme.colorScheme.outline),
+                    Expanded(
+                      // fit: BoxFit.contain,
+                      child: Text(
+                        l10n.signUpHiddenInfoDesc,
                         softWrap: true,
-                        style: theme.textTheme.headlineSmall?.copyWith(
-                          color: theme.colorScheme.primary,
+                        style: theme.textTheme.bodySmall?.copyWith(
+                          color: theme.colorScheme.outline,
                         ),
                       ),
-                      const SizedBox(height: Insets.paddingMedium),
-                      TextFormFieldWidget(
-                          textController: emailTextController,
-                          label: l10n.emailAddress,
-                          onPressed: (value) {
-                            setState(() {
-                              email = value;
-                            });
-                          },
-                          obscureText: false),
-                      const Spacer(),
-                      Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: Insets.paddingExtraLarge * 2),
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Icon(Icons.lock_outline,
-                                  color: theme.colorScheme.outline),
-                              Expanded(
-                                  // fit: BoxFit.contain,
-                                  child: Text(l10n.signUpHiddenInfoDesc,
-                                      softWrap: true,
-                                      style: theme.textTheme.bodySmall
-                                          ?.copyWith(
-                                              color:
-                                                  theme.colorScheme.outline))),
-                            ],
-                          )),
-                      SignUpBottomButtons(
-                          nextRoute: Routes.entrepreneurOrMentor.path),
-                    ]))));
+                    ),
+                  ],
+                ),
+              ),
+              const SignUpBottomButtons(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
