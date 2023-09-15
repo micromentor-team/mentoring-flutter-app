7a679fba331e95e9dd7b06161245865d8fb924d5
Rupal Gupta <63002494+rupalg@users.noreply.github.com>
2023-09-15 09:52:03 -0700

    Multi-select chips widget: retrieve the value for selected widgets AND logic for max number of selected chips (#136)
    This PR implements the functionality such that "Tapping a chip selects
    it, and stores the value somewhere in the widget where it can be
    retrieved by the parent widget."
    
    This is important for retrieving and sending the information from this
    form to the necessary db. You can test this functionality by commenting
    out these three lines in the build method:
        // var selectedChips = findSelectedChips(chipList);
        // var names = returnSelectedChipNames(selectedChips);
        // print(names);
    
    ---------
    
    Co-authored-by: Rupal Gupta <rupalgupta99@gmail.com>

--- lib/widgets/molecules/multi_select_chips.dart
+++ lib/widgets/molecules/multi_select_chips.dart
@@ -5,24 +5,26 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 //class chip
 class Chip {
   final String chipName;
+  final String textId;
   final IconData? icon;
 
   Chip({
     required this.chipName,
+    required this.textId,
     this.icon,
   });
 }
 
-//example
+//example 1
 CreateMultiSelectChips createMultiSelectChipsExample() {
   return CreateMultiSelectChips(
     chips: [
-      Chip(chipName: 'Weekly check-ins'),
-      Chip(chipName: 'Monthly check-ins'),
-      Chip(chipName: 'One-off sessions'),
-      Chip(chipName: 'Informal chats'),
-      Chip(chipName: 'Formal meetings'),
-      Chip(chipName: 'Long term'),
+      Chip(chipName: 'Weekly check-ins', textId: 'weeklyCheckIn'),
+      Chip(chipName: 'Monthly check-ins', textId: 'monthlyCheckIn'),
+      Chip(chipName: 'One-off sessions', textId: 'oneOffSessions'),
+      Chip(chipName: 'Informal chats', textId: 'informalChats'),
+      Chip(chipName: 'Formal meetings', textId: 'formatMeetings'),
+      Chip(chipName: 'Long term', textId: 'longTerm'),
     ],
   );
 }
@@ -31,24 +33,40 @@ CreateMultiSelectChips createMultiSelectChipsExample() {
 CreateMultiSelectChips createMultiSelectChipsExampleWithIcon() {
   return CreateMultiSelectChips(
     chips: [
-      Chip(chipName: 'Administrative Services', icon: Icons.work_outline),
-      Chip(chipName: 'Agriculture & Forestry', icon: Icons.work_outline),
-      Chip(chipName: 'Architecture & Engineering', icon: Icons.work_outline),
+      Chip(
+          chipName: 'Administrative Services',
+          textId: 'administrativeServices',
+          icon: Icons.work_outline),
+      Chip(
+          chipName: 'Agriculture & Forestry',
+          textId: 'agricultureAndForestry',
+          icon: Icons.work_outline),
+      Chip(
+          chipName: 'Architecture & Engineering',
+          textId: 'architectureAndEngineering',
+          icon: Icons.work_outline),
       Chip(
           chipName: 'Arts, Entertainment, & Recreation',
+          textId: 'artsEntertainmentAndRecreation',
           icon: Icons.work_outline),
-      Chip(chipName: 'Beauty, Hair, & Cosmetics', icon: Icons.work_outline),
       Chip(
-          chipName: 'Building & Grounds Maintenance', icon: Icons.work_outline),
+          chipName: 'Beauty, Hair, & Cosmetics',
+          textId: 'beautyHairAndCosmetics',
+          icon: Icons.work_outline),
+      Chip(
+          chipName: 'Building & Grounds Maintenance',
+          textId: 'buildingAndGroundsMaintenance',
+          icon: Icons.work_outline),
     ],
     maxSelection: 3,
   );
 }
 
 //build
-class CreateMultiSelectChips extends StatelessWidget {
+class CreateMultiSelectChips extends StatefulWidget {
   final List<Chip> chips;
   final int? maxSelection;
+
   const CreateMultiSelectChips({
     Key? key,
     required this.chips,
@@ -56,26 +74,82 @@ class CreateMultiSelectChips extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<CreateMultiSelectChips> createState() => _CreateMultiSelectChipsState();
+}
+
+class _CreateMultiSelectChipsState extends State<CreateMultiSelectChips> {
+  final Set<int> _childIsSelected = {};
+
+  // Callback function to be called by the child widget when selected
+  void handleChildSelection(int chipId) {
+    setState(() {
+      if (_childIsSelected.contains(chipId)) {
+        _childIsSelected.remove(chipId);
+      } else if (widget.maxSelection != null) {
+        if (_childIsSelected.length < widget.maxSelection!) {
+          _childIsSelected.add(chipId);
+        }
+      } else {
+        _childIsSelected.add(chipId);
+      }
+    });
+  }
+
+  //Helper2: input the list of all chips and returns a list of selected chips
+  List<FilterChipWidget> findSelectedChips(List<FilterChipWidget> allChips) {
+    List<FilterChipWidget> selectedChip = [];
+    for (var chip in allChips) {
+      if (chip.selected == true) {
+        selectedChip.add(chip);
+      }
+    }
+    return selectedChip;
+  }
+
+  //Helper2: input a list of selected chips, return a list of selected chips' text IDs
+  List<String?> returnSelectedChipNames(List<FilterChipWidget> selectedChips) {
+    List<String> textIds = [];
+    for (var chip in selectedChips) {
+      if (chip.selected == true) {
+        textIds.add(chip.textId);
+      }
+    }
+    return textIds;
+  }
+
+  @override
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
     final AppLocalizations l10n = AppLocalizations.of(context)!;
 
-    String textString = maxSelection != null
-        ? l10n.profileChipsSelectUpTo(maxSelection!)
+    String textString = widget.maxSelection != null
+        ? l10n.profileChipsSelectUpTo(widget.maxSelection!)
         : l10n.profileChipsSelectAllThatApply;
 
     List<Widget> chipListWithPadding = [];
-    for (int i = 0; i < chips.length; i++) {
+    List<FilterChipWidget> chipList = [];
+    for (int i = 0; i < widget.chips.length; i++) {
       var chip = FilterChipWidget(
-        chipName: chips[i].chipName,
-        icon: chips[i].icon,
+        id: i,
+        selected: _childIsSelected.contains(i),
+        chipName: widget.chips[i].chipName,
+        textId: widget.chips[i].textId,
+        icon: widget.chips[i].icon,
+        onSelectionChanged: handleChildSelection,
       );
+      chipList.add(chip);
       chipListWithPadding.add(chip);
       var padding = const SizedBox(
         height: Insets.paddingMedium,
       );
       chipListWithPadding.add(padding);
     }
+
+    // Example of how to use the 2 helper functions in the build method:
+    // var selectedChips = findSelectedChips(chipList);
+    // var textIds = returnSelectedChipNames(selectedChips);
+    // print(textIds);
+
     return Column(
       children: [
         Center(
@@ -97,51 +171,51 @@ class CreateMultiSelectChips extends StatelessWidget {
   }
 }
 
-class FilterChipWidget extends StatefulWidget {
+class FilterChipWidget extends StatelessWidget {
+  final int id;
   final String chipName;
+  final String textId;
   final IconData? icon;
+  final bool selected;
+  final Function(int chipId) onSelectionChanged;
 
-  const FilterChipWidget({
-    Key? key,
-    required this.chipName,
-    this.icon,
-  }) : super(key: key);
-
-  @override
-  State<FilterChipWidget> createState() => _FilterChipWidgetState();
-}
-
-class _FilterChipWidgetState extends State<FilterChipWidget> {
-  var _isSelected = false;
+  const FilterChipWidget(
+      {Key? key,
+      required this.id,
+      required this.textId,
+      required this.chipName,
+      this.icon,
+      this.selected = false,
+      required this.onSelectionChanged})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
+
     return FilterChip(
-      avatar: widget.icon != null
+      avatar: icon != null
           ? Icon(
-              widget.icon!,
-              color: _isSelected
+              icon!,
+              color: selected
                   ? theme.colorScheme.onSurfaceVariant
                   : theme.colorScheme.onSecondaryContainer,
             )
           : null,
-      label: Text(widget.chipName),
+      label: Text(chipName),
       labelStyle: theme.textTheme.labelLarge?.copyWith(
-        color: _isSelected
+        color: selected
             ? theme.colorScheme.onSurfaceVariant
             : theme.colorScheme.onSecondaryContainer,
       ),
       selectedColor: theme.colorScheme.secondaryContainer,
-      selected: _isSelected,
-      side: _isSelected == false
+      selected: selected,
+      side: selected == false
           ? BorderSide(color: theme.colorScheme.outline)
           : null,
       showCheckmark: false,
       onSelected: (bool selected) {
-        setState(() {
-          _isSelected = selected;
-        });
+        onSelectionChanged(id);
       },
     );
   }
