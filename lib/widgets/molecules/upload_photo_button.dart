37e19b7e840c44b2e10db0582df9c64d8786044c
Rupal Gupta <rupalgupta99@gmail.com>
2023-09-14 17:52:14 -0700

    photo upload widget

--- lib/widgets/molecules/upload_photo_button.dart
+++ lib/widgets/molecules/upload_photo_button.dart
@@ -9,14 +9,82 @@ class UploadPhotoButton extends StatelessWidget {
     final ThemeData theme = Theme.of(context);
 
     return OutlinedButton(
-      onPressed: null,
+      onPressed: () {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0)),
+              backgroundColor: theme.colorScheme.secondaryContainer,
+              contentPadding: EdgeInsets.zero,
+              content: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16.0, bottom: 8.0),
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        foregroundColor: MaterialStatePropertyAll(
+                            theme.colorScheme.onSecondaryContainer),
+                        side: const MaterialStatePropertyAll(BorderSide.none),
+                      ),
+                      child: const Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(
+                            Icons.insert_photo_outlined,
+                          ),
+                          SizedBox(width: 8),
+                          Text("Upload photo"),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const Divider(),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 16.0, top: 8.0),
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        foregroundColor: MaterialStatePropertyAll(
+                            theme.colorScheme.onSecondaryContainer),
+                        side: const MaterialStatePropertyAll(BorderSide.none),
+                      ),
+                      child: const Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(
+                            Icons.camera_alt_outlined,
+                          ),
+                          SizedBox(width: 8),
+                          Text("Take photo"),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      },
       style: ButtonStyle(
+        shape: MaterialStatePropertyAll(
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0))),
         backgroundColor:
             MaterialStatePropertyAll(theme.colorScheme.secondaryContainer),
+        side: const MaterialStatePropertyAll(BorderSide.none),
       ),
       child: Container(
-        child: Icon(Icons.add_circle_outline,
-            color: theme.colorScheme.onSecondaryContainer),
+        width: 200,
+        height: 200,
+        child: Center(
+          child: Icon(Icons.add_circle_outline,
+              color: theme.colorScheme.onSecondaryContainer),
+        ),
       ),
     );
   }
