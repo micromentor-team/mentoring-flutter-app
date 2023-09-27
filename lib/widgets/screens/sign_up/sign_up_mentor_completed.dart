0f37c449bc0c3e2716382ccb8dbefc65e83c156d
Rupal Gupta <rupalgupta99@gmail.com>
2023-09-27 14:53:55 -0700

    last screen

--- lib/widgets/screens/sign_up/sign_up_mentor_completed.dart
+++ lib/widgets/screens/sign_up/sign_up_mentor_completed.dart
@@ -19,6 +19,7 @@ class _SignupMentorCompletedScreenState
   @override
   Widget build(BuildContext context) {
     final AppLocalizations l10n = AppLocalizations.of(context)!;
+    final ThemeData theme = Theme.of(context);
 
     return SignUpTemplate(
       progress: SignUpProgress.four,
@@ -32,9 +33,39 @@ class _SignupMentorCompletedScreenState
           rightOnPress: () {
             context.push(Routes.signupMentorInternationally.path);
           }),
-      footer: SignUpIconFooter(
-          icon: Icons.visibility_outlined, text: l10n.signUpShownOnProfileInfo),
-      body: const Placeholder(),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 240,
+            child: ColorFiltered(
+                colorFilter: ColorFilter.mode(
+                  theme.colorScheme.onSurface,
+                  BlendMode.srcATop,
+                ),
+                child: const Image(
+                  image: AssetImage(Assets.signUpCompletedScreenStockImage),
+                )),
+          ),
+          const SizedBox(
+            height: Insets.paddingMedium,
+          ),
+          Text(
+            l10n.signUpCompletedTextOne,
+            style: theme.textTheme.bodyMedium!
+                .copyWith(color: theme.colorScheme.outline),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(
+            height: Insets.paddingSmall,
+          ),
+          Text(
+            l10n.signUpCompletedTextTwoMentor,
+            style: theme.textTheme.bodyMedium!
+                .copyWith(color: theme.colorScheme.outline),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
     );
   }
 }
