20a5b625801ee2cf9a6605fbf3baf57697770094
Miguel Rosario <miguel.a.rosario.aponte@gmail.com>
2023-10-06 10:39:56 -0700

    Channel messages infinite scrolling with lazy loading

--- lib/constants/numerical.dart
+++ lib/constants/numerical.dart
@@ -3,6 +3,7 @@ part of 'app_constants.dart';
 class Limits {
   Limits._private();
   static const maxNotificationsDisplayed = 99;
+  static const chatMessagesPageSize = 25;
   static const searchResultsBatchSize = 100;
   static const searchResultsPageSize = 5;
 }
