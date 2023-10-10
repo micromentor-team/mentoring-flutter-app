b041eb7fdb05629dee94e7798ee6c2723256269b
Rupal Gupta <rupalgupta99@gmail.com>
2023-10-09 23:37:39 -0700

    mentors for you section

--- lib/widgets/atoms/mentor_card.dart
+++ lib/widgets/atoms/mentor_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:mm_flutter_app/constants/app_constants.dart';
+import 'package:mm_flutter_app/widgets/atoms/skill_chip.dart';
 
 class MentorCard extends StatelessWidget {
   final String? avatarUrl;
@@ -10,7 +11,6 @@ class MentorCard extends StatelessWidget {
   final List<String> mentorSkill;
   final void Function()? onTap;
 
-  static const double _mentorCardHeight = 240;
   static const double _mentorCardWidth = 296;
 
   const MentorCard({
@@ -59,16 +59,14 @@ class MentorCard extends StatelessWidget {
           //using SizedBox to prevent Text Overflow beyond the Card
           child: SizedBox(
             width: _mentorCardWidth / 2,
-            child: Flexible(
-              child: Text(
-                mentorName,
-                style: theme.textTheme.titleSmall?.copyWith(
-                  color: theme.colorScheme.onSurface,
-                ),
-                softWrap: true,
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
+            child: Text(
+              mentorName,
+              style: theme.textTheme.titleSmall?.copyWith(
+                color: theme.colorScheme.onSurface,
               ),
+              softWrap: true,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
             ),
           ),
         ),
@@ -80,16 +78,14 @@ class MentorCard extends StatelessWidget {
           //using SizedBox to prevent Text Overflow beyond the Card
           child: SizedBox(
             width: _mentorCardWidth / 2,
-            child: Flexible(
-              child: Text(
-                mentorBio,
-                style: theme.textTheme.labelSmall?.copyWith(
-                  color: theme.colorScheme.onSurface,
-                ),
-                softWrap: true,
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
+            child: Text(
+              mentorBio,
+              style: theme.textTheme.labelSmall?.copyWith(
+                color: theme.colorScheme.onSurface,
               ),
+              softWrap: true,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
             ),
           ),
         ),
@@ -112,16 +108,14 @@ class MentorCard extends StatelessWidget {
                       color: theme.colorScheme.onSurface,
                     ),
                   ),
-                  Flexible(
-                    child: Text(
-                      l10n.exploreEndorsements(mentorEndorsements!),
-                      style: theme.textTheme.labelSmall?.copyWith(
-                        color: theme.colorScheme.onSurface,
-                      ),
-                      softWrap: true,
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
+                  Text(
+                    l10n.exploreEndorsements(mentorEndorsements!),
+                    style: theme.textTheme.labelSmall?.copyWith(
+                      color: theme.colorScheme.onSurface,
                     ),
+                    softWrap: true,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
                   ),
                 ],
               ),
@@ -136,7 +130,7 @@ class MentorCard extends StatelessWidget {
     final AppLocalizations l10n = AppLocalizations.of(context)!;
 
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Padding(
           padding: const EdgeInsets.all(Insets.paddingSmall),
@@ -150,56 +144,83 @@ class MentorCard extends StatelessWidget {
             overflow: TextOverflow.ellipsis,
           ),
         ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(Insets.paddingSmall, 0,
+              Insets.paddingSmall, Insets.paddingMedium),
+          child: SizedBox(
+            width: _mentorCardWidth,
+            child: Wrap(
+              children: _createSkillChips(context),
+            ),
+          ),
+        )
       ],
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    Column avatarColumn = _getAvatar();
-    Column mentorInfoColumn = _getMentorInfo(context);
-    Column mentorExpertiseColumn = _getMentorExpertiseColumn(context);
+  List<Widget> _createSkillChips(context) {
     final ThemeData theme = Theme.of(context);
 
-    return Padding(
-      padding: const EdgeInsetsDirectional.only(end: Insets.paddingSmall),
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: theme.colorScheme.outlineVariant,
+    List<Widget> rowChildren = [];
+    for (String expertise in mentorSkill) {
+      rowChildren.addAll([
+        ExpertiseChip(
+          expertise: expertise,
+          icon: Icon(
+            Icons.campaign_outlined,
+            color: theme.colorScheme.onSecondaryContainer,
           ),
-          borderRadius: BorderRadius.circular(Radii.roundedRectRadiusSmall),
         ),
-        child: Card(
-          elevation: 0,
-          color: Colors.transparent,
-          child: InkWell(
-            onTap: onTap,
-            child: SizedBox(
-              width: _mentorCardWidth,
-              height: _mentorCardHeight,
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      avatarColumn,
-                      mentorInfoColumn,
-                    ],
+        const SizedBox(width: Insets.paddingExtraSmall),
+      ]);
+    }
+    return rowChildren;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Column mentorExpertiseColumn = _getMentorExpertiseColumn(context);
+    final ThemeData theme = Theme.of(context);
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+
+    return Card(
+      elevation: Elevations.level0,
+      surfaceTintColor: Colors.transparent,
+      color: colorScheme.surface,
+      child: InkWell(
+        onTap: onTap,
+        child: Ink(
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: theme.colorScheme.outlineVariant,
+              width: Dimensions.highlightBorderWidth,
+            ),
+            borderRadius: BorderRadius.circular(Radii.roundedRectRadiusMedium),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(Insets.paddingExtraSmall),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    _getAvatar(),
+                    _getMentorInfo(context),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsetsDirectional.only(
+                      start: Insets.paddingMedium, end: Insets.paddingMedium),
+                  child: Divider(
+                    color: theme.colorScheme.outlineVariant,
                   ),
-                  Padding(
-                    padding: const EdgeInsetsDirectional.only(
-                        start: Insets.paddingMedium, end: Insets.paddingMedium),
-                    child: Divider(
-                      color: theme.colorScheme.outlineVariant,
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      mentorExpertiseColumn,
-                    ],
-                  ),
-                ],
-              ),
+                ),
+                Row(
+                  children: [
+                    mentorExpertiseColumn,
+                  ],
+                ),
+              ],
             ),
           ),
         ),
--- lib/widgets/molecules/recommended_mentors_scroll.dart
+++ lib/widgets/molecules/recommended_mentors_scroll.dart
@@ -9,6 +9,97 @@ import '../../__generated/schema/schema.graphql.dart';
 import '../../providers/base/operation_result.dart';
 import '../../providers/user_provider.dart';
 
+class RecommendedSection extends StatefulWidget {
+  final AuthenticatedUser authenticatedUser;
+  const RecommendedSection({
+    Key? key,
+    required this.authenticatedUser,
+  }) : super(key: key);
+
+  @override
+  State<RecommendedSection> createState() => _RecommendedSectionState();
+}
+
+class _RecommendedSectionState extends State<RecommendedSection> {
+  late final UserProvider _userProvider;
+  late Future<OperationResult<List<MentorUser>>> _recommendedMentors;
+
+  @override
+  void initState() {
+    super.initState();
+    _userProvider = Provider.of<UserProvider>(context, listen: false);
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _recommendedMentors = _userProvider.findMentorUsers(
+      optionsInput: Input$FindObjectsOptions(limit: 20),
+      filterInput: Input$UserListFilter(
+        ids: [
+          // TODO - Test Users, remove when matching engine is ready
+          '64ee5cf957728fa723e28d59',
+          '64ee5f4557728fa723e28d60',
+          '64ee606457728fa723e28d67',
+          '64ee618e57728fa723e28d6e',
+          '64ee629a57728fa723e28d75',
+          '64ee63fe57728fa723e28d7c',
+          '64ee654357728fa723e28d83',
+          '64ee660957728fa723e28d8a',
+          '64ee670457728fa723e28d91',
+          '64ee68d457728fa723e28d98',
+          '64ee6f8357728fa723e28d9f',
+          '64ee706157728fa723e28da6',
+          '64ee710357728fa723e28dad',
+          '64ee71b557728fa723e28db4',
+          '64ee726f57728fa723e28dbb',
+          '64ee733557728fa723e28dc2',
+          '64ee73e657728fa723e28dc9',
+          '64ee749457728fa723e28dd0',
+          '64ee758a57728fa723e28dd7',
+          '64ee768757728fa723e28dde',
+        ],
+      ), // no filter/null filter
+      matchInput: Input$UserInput(), // empty input, won't restrict matches
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _recommendedMentors,
+      builder: (context, snapshot) {
+        return AppUtility.widgetForAsyncSnapshot(
+          snapshot: snapshot,
+          onReady: () {
+            List<MentorUser> mentors = snapshot.data?.response != null
+                ? snapshot.data!.response!
+                    .where(
+                        (element) => element.id != widget.authenticatedUser.id)
+                    .toList()
+                : [];
+            return _createRecommendedMentorsWidget(mentors, context);
+          },
+        );
+      },
+    );
+  }
+
+  Column _createRecommendedMentorsWidget(
+      List<MentorUser> mentors, BuildContext context) {
+    final AppLocalizations l10n = AppLocalizations.of(context)!;
+    return Column(
+      children: [
+        RecommendedMentorsHeading(
+          title: l10n.mentorsForYou,
+          subtitle: l10n.recommendationsBasedOnYourProfile,
+        ),
+        RecommendedMentorsScroll(mentors: mentors),
+      ],
+    );
+  }
+}
+
 class RecommendedMentorsScroll extends StatelessWidget {
   final List<MentorUser> mentors;
 
@@ -149,97 +240,6 @@ class _SectionExpandToggle extends StatelessWidget {
           ),
         ),
       ),
-    );
-  }
-}
-
-class RecommendedSection extends StatefulWidget {
-  final AuthenticatedUser authenticatedUser;
-  const RecommendedSection({
-    Key? key,
-    required this.authenticatedUser,
-  }) : super(key: key);
-
-  @override
-  State<RecommendedSection> createState() => _RecommendedSectionState();
-}
-
-class _RecommendedSectionState extends State<RecommendedSection> {
-  late final UserProvider _userProvider;
-  late Future<OperationResult<List<MentorUser>>> _recommendedMentors;
-
-  @override
-  void initState() {
-    super.initState();
-    _userProvider = Provider.of<UserProvider>(context, listen: false);
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    _recommendedMentors = _userProvider.findMentorUsers(
-      optionsInput: Input$FindObjectsOptions(limit: 20),
-      filterInput: Input$UserListFilter(
-        ids: [
-          // TODO - Test Users, remove when matching engine is ready
-          '64ee5cf957728fa723e28d59',
-          '64ee5f4557728fa723e28d60',
-          '64ee606457728fa723e28d67',
-          '64ee618e57728fa723e28d6e',
-          '64ee629a57728fa723e28d75',
-          '64ee63fe57728fa723e28d7c',
-          '64ee654357728fa723e28d83',
-          '64ee660957728fa723e28d8a',
-          '64ee670457728fa723e28d91',
-          '64ee68d457728fa723e28d98',
-          '64ee6f8357728fa723e28d9f',
-          '64ee706157728fa723e28da6',
-          '64ee710357728fa723e28dad',
-          '64ee71b557728fa723e28db4',
-          '64ee726f57728fa723e28dbb',
-          '64ee733557728fa723e28dc2',
-          '64ee73e657728fa723e28dc9',
-          '64ee749457728fa723e28dd0',
-          '64ee758a57728fa723e28dd7',
-          '64ee768757728fa723e28dde',
-        ],
-      ), // no filter/null filter
-      matchInput: Input$UserInput(), // empty input, won't restrict matches
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _recommendedMentors,
-      builder: (context, snapshot) {
-        return AppUtility.widgetForAsyncSnapshot(
-          snapshot: snapshot,
-          onReady: () {
-            List<MentorUser> mentors = snapshot.data?.response != null
-                ? snapshot.data!.response!
-                    .where(
-                        (element) => element.id != widget.authenticatedUser.id)
-                    .toList()
-                : [];
-            return _createRecommendedMentorsWidget(mentors, context);
-          },
-        );
-      },
-    );
-  }
-
-  Column _createRecommendedMentorsWidget(
-      List<MentorUser> mentors, BuildContext context) {
-    final AppLocalizations l10n = AppLocalizations.of(context)!;
-    return Column(
-      children: [
-        RecommendedMentorsHeading(
-          title: l10n.mentorsForYou,
-          subtitle: l10n.recommendationsBasedOnYourProfile,
-        ),
-        RecommendedMentorsScroll(mentors: mentors),
-      ],
     );
   }
 }
