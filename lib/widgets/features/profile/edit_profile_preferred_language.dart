d422a094ff94f24e80b8b8b09e26b985e3d60e77
Rupal Gupta <rupalgupta99@gmail.com>
2023-10-17 16:44:28 -0700

    completed the UI of the preferred language page. TODO: need to figure out how to restrict the preferred language selection to 1

--- lib/widgets/features/profile/edit_profile_preferred_language.dart
+++ lib/widgets/features/profile/edit_profile_preferred_language.dart
@@ -1,22 +1,77 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:mm_flutter_app/constants/app_constants.dart';
+import 'package:provider/provider.dart';
+import 'package:textfield_tags/textfield_tags.dart';
+import '../../../providers/content_provider.dart';
+import '../../../providers/models/user_registration_model.dart';
+import '../../shared/autocomplete_picker.dart';
 import 'edit_template.dart';
 
-class EditPreferredLanguageScreen extends StatelessWidget {
+class EditPreferredLanguageScreen extends StatefulWidget {
   const EditPreferredLanguageScreen({Key? key}) : super(key: key);
+
+  @override
+  State<EditPreferredLanguageScreen> createState() =>
+      _EditPreferredLanguageScreenState();
+}
+
+class _EditPreferredLanguageScreenState
+    extends State<EditPreferredLanguageScreen> {
+  final _preferredLanguagesController = TextfieldTagsController();
+  late final ContentProvider _contentProvider;
+  late final UserRegistrationModel _registrationModel;
+  bool _selectedPreferredLanguage = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _contentProvider = Provider.of<ContentProvider>(context, listen: false);
+    _registrationModel = Provider.of<UserRegistrationModel>(
+      context,
+      listen: false,
+    );
+    _preferredLanguagesController.addListener(() {
+      setState(() {
+        _selectedPreferredLanguage = _preferredLanguagesController.hasTags;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    try {
+      _preferredLanguagesController.dispose();
+    } catch (_) {}
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final AppLocalizations l10n = AppLocalizations.of(context)!;
+    final ThemeData theme = Theme.of(context);
 
     return EditTemplate(
       title: l10n.profileEditLanguagePreferred,
-      body: const Padding(
-        padding: EdgeInsets.all(Insets.paddingMedium),
+      body: Padding(
+        padding: const EdgeInsets.all(Insets.paddingMedium),
         child: Column(
           children: [
-            Placeholder(),
+            Padding(
+              padding: const EdgeInsets.all(Insets.paddingSmall),
+              child: Text(
+                l10n.profileEditLanguagePreferredSubtitle,
+                style: theme.textTheme.bodyMedium!
+                    .copyWith(color: theme.colorScheme.secondary),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            AutocompletePicker(
+              controller: _preferredLanguagesController,
+              options: _contentProvider.languageOptions!
+                  .map((e) => e.translatedValue!)
+                  .toList(),
+            ),
           ],
         ),
       ),
