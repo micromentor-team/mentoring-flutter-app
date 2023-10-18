aaf3cddedfd4ecaa64ac87aa772352573801a4c7
Rupal Gupta <rupalgupta99@gmail.com>
2023-10-17 17:14:22 -0700

    completed the UI of the pronoun edit page

--- lib/widgets/features/profile/edit_profile_pronoun.dart
+++ lib/widgets/features/profile/edit_profile_pronoun.dart
@@ -1,10 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:mm_flutter_app/constants/app_constants.dart';
+import 'package:provider/provider.dart';
+import '../../../providers/content_provider.dart';
+import '../sign_up/components/checkbox_list_and_form.dart';
 import 'edit_template.dart';
 
-class EditProfilePronounScreen extends StatelessWidget {
+class EditProfilePronounScreen extends StatefulWidget {
   const EditProfilePronounScreen({Key? key}) : super(key: key);
+
+  @override
+  State<EditProfilePronounScreen> createState() =>
+      _EditProfilePronounScreenState();
+}
+
+class _EditProfilePronounScreenState extends State<EditProfilePronounScreen> {
+  late final ContentProvider _contentProvider;
+  final List<String> _selections = List.empty(growable: true);
+
+  @override
+  void initState() {
+    super.initState();
+    _contentProvider = Provider.of<ContentProvider>(context, listen: false);
+  }
+
+  List<LabeledCheckbox> _createCheckboxes() {
+    return _contentProvider.presetPronounOptions!
+        .map(
+          (e) => _createLabeledCheckbox(e.translatedValue!, e.textId),
+        )
+        .toList();
+  }
+
+  LabeledCheckbox _createLabeledCheckbox(String label, String textId) {
+    return LabeledCheckbox(
+      label: label,
+      id: textId,
+      value: _selections.contains(textId),
+      selectionOrder: _selections.indexOf(textId) + 1,
+      onChanged: (bool isSelected) {
+        setState(() {
+          if (isSelected) {
+            _selections.add(textId);
+          } else {
+            _selections.remove(textId);
+          }
+        });
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,6 +67,10 @@ class EditProfilePronounScreen extends StatelessWidget {
                   .copyWith(color: theme.colorScheme.outline),
               textAlign: TextAlign.center,
             ),
+            const SizedBox(
+              height: Insets.paddingMedium,
+            ),
+            ..._createCheckboxes(),
           ],
         ),
       ),
